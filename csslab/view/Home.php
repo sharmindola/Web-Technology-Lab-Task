cbb8220c1f63ffe3378aa90a16fccd801a5e85db
Samia Sharmin Dola <113165235+sharmindola@users.noreply.github.com>
2024-09-28 23:40:47 +0600

    Add files via upload

---
+++ csslab/view/Home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+  <title>Learning CSS</title>
+  <link rel="stylesheet" type="text/css"  href="../css/labtask.css">
+
+</head>
+<body>
+<div id="header">
+    <h1>My First CSS Example</h1>
+    <p class="paragraph2">This is a paragraph.</p>
+</div>
+    <p>This is a paragraph.</p>
+    <p>This is a paragraph.</p>
+    <h2 class= "para5">This is a paragraph.</h2>
+    <h3 class="paragraph2">This is a paragraph.</h3>
+    <h3 class="para3">This is a paragraph.</h3>
+    <h4 class= "para4">This is a paragraph.</h4>
+</body>
+ 
+</html>
