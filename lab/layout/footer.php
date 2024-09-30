e345546e02137966dd3e9a7ee3689864edf648f9
Samia Sharmin Dola <113165235+sharmindola@users.noreply.github.com>
2024-09-30 16:25:05 +0600

    Add files via upload

---
+++ lab/layout/footer.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<footer>
+  <p>Author: Copy & Paste<br>
+  <a href="mailto:jhtuhin7709@gmail.com">jhtuhin7709@gmail.com</a></p>
+</footer>
+</body>
+</html>
---
+++ lab/layout/header.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+<img src="../images/img.jpg">
+
+<div class="sticky">
+    <table>
+        <tr>
+            <td><a href="">Home</a></td>
+            <td><a href="">Create Order</a></td>
+            <td>Show Order</td>
+        </tr>
+</table>
+</div>
+
+
+</body>
+
+</html>
